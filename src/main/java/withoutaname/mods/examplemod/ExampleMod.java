eeecaad6cf98b4219eec391137bd2c94f6a330e0
WithoutAName25 <withoutaname25@gmail.com>
2021-07-24 16:19:49 +0200

    Initial commit

---
+++ src/main/java/withoutaname/mods/examplemod/ExampleMod.java
@@ -0,0 +1,22 @@
+package withoutaname.mods.examplemod;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(ExampleMod.MODID)
+@Mod.EventBusSubscriber
+public class ExampleMod {
+    public static final String MODID = "examplemod";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public ExampleMod() {}
+
+    @SubscribeEvent
+    public static void onServerStarting(FMLServerStartingEvent event) {
+        // do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+}
